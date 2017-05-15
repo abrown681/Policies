b748f7e6643ef095d5f587c19b655faa7786cf7e
abrown681 <austinb681@gmail.com>
2017-05-15 11:59:46 -0400

    Modified templates to display rules from database.

--- policies/models.py
+++ policies/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 class Rule(models.Model):
     author = models.ForeignKey('auth.User')
     name = models.CharField(max_length=50, verbose_name='Policy name')
-    priority = models.PositiveSmallIntegerField(verbose_name='Priority level')
+    priority = models.PositiveSmallIntegerField(verbose_name='Priority level', null=True)
     day = models.CharField(max_length=10, blank=True, null=True, verbose_name='Day requirement')
     US_Government = models.BooleanField()
-    created_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     UNCLASSIFIED = 'UC'
@@ -50,4 +50,4 @@ class Rule(models.Model):
         self.save
 
     def __str__(self):
-        return self.title
+        return self.name
--- policies/views.py
+++ policies/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Rule
 
 def policies_list(request):
-    return render(request, 'policies/policies_list.html', {})
+    rules = Rule.objects.order_by('priority')
+    return render(request, 'policies/policies_list.html', {'rules': rules})
 
 # Create your views here.
